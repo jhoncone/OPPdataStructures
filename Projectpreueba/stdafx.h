865801c454c76fe57130e02a3d91fcf99cb7b8ec
jhoncone <40128739+jhoncone@users.noreply.github.com>
2021-02-10 21:40:02 -0600

    Delete stdafx.h

--- Projectpreueba/stdafx.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-using namespace System;
-using namespace System::ComponentModel;
-using namespace System::Collections;
-using namespace System::Windows::Forms;
-using namespace System::Data;
-using namespace System::Drawing;
-
-//private: System::Void btnLoad_Click(System::Object^ sender, System::EventArgs^ e);
