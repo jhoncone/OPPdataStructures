bb81b726756a006855a5dced71462e08d6b34823
jhoncone <40128739+jhoncone@users.noreply.github.com>
2021-02-10 21:40:17 -0600

    Add files via upload

---
+++ Projectpreueba/stdafx.h
@@ -0,0 +1,9 @@
+#pragma once
+using namespace System;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+using namespace System::Windows::Forms;
+using namespace System::Data;
+using namespace System::Drawing;
+
+//private: System::Void btnLoad_Click(System::Object^ sender, System::EventArgs^ e);
